0adb83d46b966e9dad28884e1cd126b62f7e32c1
tomi <tomipaul95@gmail.com>
2017-05-02 10:01:20 +0100

    [chore]: Update gulpfile.babel.js
    Add pre-test gulp task
    -- Declare files for test coverage
    Add coverage gulp task
    -- Generate test reports and send to coverall for analysis

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,9 +3,20 @@ import jasmineNode from 'gulp-jasmine-node';
 import nodemon from 'gulp-nodemon';
 import babel from 'gulp-babel';
 import rename from 'gulp-rename';
+import istanbul from 'gulp-istanbul';
+import coveralls from 'gulp-coveralls';
 
-gulp.task('run-tests', ['babelifyTestFiles'], () => {
-  gulp.src('spec/*.js')
+gulp.task('pre-test', () => {
+  return gulp.src('dist/*.js')
+  // Covering files
+  .pipe(istanbul({ includeUntested: true }))
+  // Force `require` to return covered files
+  .pipe(istanbul.hookRequire());
+});
+
+gulp.task('run-tests', ['pre-test'], () => {
+  return gulp.src('spec/*.js')
+  // Run test assertions with jasmine-node
   .pipe(jasmineNode({
     timeout: 10000,
     includeStackTrace: true,
@@ -14,24 +25,25 @@ gulp.task('run-tests', ['babelifyTestFiles'], () => {
 });
 
 gulp.task('babelifyTestFiles', () => {
-  const toCamelCaseSpec = (filename) => {
+  // rename test file to match /spec\.js$/i
+  const renameTestFile = (filename) => {
     const replaceFunc = (match, p) => {
       return String.prototype.toUpperCase.call(p);
     };
     return String.prototype.replace.call(filename, /-([a-z])/g, replaceFunc);
   };
-  gulp.src('tests/*.js')
+  return gulp.src('tests/*.js')
   .pipe(babel({
     presets: ['es2015']
   }))
   .pipe(rename((path) => {
-    path.basename = `${toCamelCaseSpec(path.basename)}Spec`;
+    path.basename = `${renameTestFile(path.basename)}Spec`;
   }))
   .pipe(gulp.dest('spec'));
 });
 
 gulp.task('babelifySrcFiles', () => {
-  gulp.src('src/*.js')
+  return gulp.src('src/*.js')
   .pipe(babel({
     presets: ['es2015']
   }))
@@ -42,7 +54,21 @@ gulp.task('serve', () => {
   nodemon({
     script: 'dist/app.js',
     ext: 'js json',
-    ignore: ['node_modules/', '.vscode/'],
+    ignore: [
+      'node_modules/',
+      '.vscode/',
+      '/test',
+      '/spec',
+      'gulpfile.babel.js'],
     tasks: ['babelifySrcFiles']
+  });
+});
+
+gulp.task('coverage', ['run-tests'], () => {
+  return gulp.src('spec/*.js')
+  .pipe(istanbul.writeReports())
+  .on('end', () => {
+    gulp.src('coverage/lcov.info')
+    .pipe(coveralls());
   });
 });
