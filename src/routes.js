3ff360470b8ccd39ad7c91f0d2a546cd00eb3e2c
tomi <tomipaul95@gmail.com>
2017-05-08 01:52:01 +0100

    [feat]: Add api endpoints

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+import InvertedIndex from './inverted-index';
+
+/**
+ * Define routes and route handlers
+ * @function apiEndPoints
+ * @param {Object} app -an express application instance
+ * @returns {Object} -index object which maps from words to book object indexes
+ */
+const apiEndPoints = function apiEndpoints(app) {
+  const invertedIndex = new InvertedIndex();
+  app.post('/api/create', (req, res) => {
+    const { fileName, fileContent } = req.body;
+    const index = invertedIndex.createIndex(fileName, fileContent);
+    if (Array.isArray(index)) {
+      return res.status(400).json(index[1]);
+    }
+    return res.status(200).json(index);
+  });
+
+  app.post('/api/search', (req, res) => {
+    const { index, fileName, terms } = req.body;
+    const termsArray = (terms) ? terms.split(', ') : [];
+    const searchResult = invertedIndex.searchIndex(index,
+     fileName, ...termsArray);
+    if (typeof searchResult === 'string') {
+      return res.status(400).json(searchResult);
+    }
+    return res.status(200).json(searchResult);
+  });
+};
+
+export default apiEndPoints;
